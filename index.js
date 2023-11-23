2e3d159b5b94110fec62f8a01a440f86d8904c28
Dzook1 <147112512+Dzook1@users.noreply.github.com>
2023-11-22 20:40:23 -0500

    Remove item function
    Removes menu item when remove button is clicked

--- index.js
+++ index.js
@@ -288,11 +288,12 @@ function retrieveItemsEntreesManager() {
 
     for (i in entreeNames) {
         let name = document.createElement('h4');
+        name.classList.add('item-name-entree')
         name.innerHTML = entreeNames.slice(0)[i];
         entreeBox.appendChild(name);
     
         let description = document.createElement('p');
-        description.classList.add('item-description');
+        description.classList.add('item-description-entree');
         description.innerHTML = entreeDescriptions.slice(0)[i];
         entreeBox.appendChild(description);
     
@@ -302,7 +303,7 @@ function retrieveItemsEntreesManager() {
         entreeBox.appendChild(price);
     
         let image = document.createElement('img');
-        image.classList.add('item-image');
+        image.classList.add('item-image-entree');
         image.src = entreeImages.slice(0)[i];
         entreeBox.appendChild(image);
     
@@ -325,8 +326,38 @@ function retrieveItemsEntreesManager() {
         entreeBox.appendChild(managePrice);
     
         let remove = document.createElement('button');
-        remove.classList.add('remove-btn');
+        remove.classList.add('remove-entree');
         remove.innerHTML = 'Remove';
+        remove.addEventListener('click', function() {
+            let removeArray = document.getElementsByClassName('remove-entree');
+            let names = document.getElementsByClassName('item-name-entree');
+            let descriptions = document.getElementsByClassName('item-description-entree');
+            let prices = document.getElementsByClassName('item-price-entree');
+            let images = document.getElementsByClassName('item-image-entree');
+            let managePriceArray = document.getElementsByClassName('managePrice-entree');
+
+            
+            let index = Array.prototype.indexOf.call(removeArray, this);
+
+            names[index].remove();
+            descriptions[index].remove();
+            prices[index].remove();
+            images[index].remove();
+            managePriceArray[index].remove();
+            removeArray[index].remove();
+
+            entreeNames.splice(index, 1);
+            localStorage.setItem('entreeNames', JSON.stringify(entreeNames));
+
+            entreeDescriptions.splice(index, 1);
+            localStorage.setItem('entreeDescriptions', JSON.stringify(entreeDescriptions));
+
+            entreePrices.splice(index, 1);
+            localStorage.setItem('entreePrices', JSON.stringify(entreePrices));
+
+            entreeImages.splice(index, 1);
+            localStorage.setItem('entreeImages', JSON.stringify(entreeImages));
+        })
         entreeBox.appendChild(remove);
     }
 }
@@ -550,11 +581,12 @@ function retrieveItemsSidesManager() {
 
     for (i in sideNames) {
         let name = document.createElement('h4');
+        name.classList.add('item-name-side')
         name.innerHTML = sideNames.slice(0)[i];
         sideBox.appendChild(name);
     
         let description = document.createElement('p');
-        description.classList.add('item-description');
+        description.classList.add('item-description-side');
         description.innerHTML = sideDescriptions.slice(0)[i];
         sideBox.appendChild(description);
     
@@ -564,7 +596,7 @@ function retrieveItemsSidesManager() {
         sideBox.appendChild(price);
     
         let image = document.createElement('img');
-        image.classList.add('item-image');
+        image.classList.add('item-image-side');
         image.src = sideImages.slice(0)[i];
         sideBox.appendChild(image);
     
@@ -587,8 +619,38 @@ function retrieveItemsSidesManager() {
         sideBox.appendChild(managePrice);
     
         let remove = document.createElement('button');
-        remove.classList.add('remove-btn');
+        remove.classList.add('remove-side');
         remove.innerHTML = 'Remove';
+        remove.addEventListener('click', function() {
+            let removeArray = document.getElementsByClassName('remove-side');
+            let names = document.getElementsByClassName('item-name-side');
+            let descriptions = document.getElementsByClassName('item-description-side');
+            let prices = document.getElementsByClassName('item-price-side');
+            let images = document.getElementsByClassName('item-image-side');
+            let managePriceArray = document.getElementsByClassName('managePrice-side');
+
+            
+            let index = Array.prototype.indexOf.call(removeArray, this);
+
+            names[index].remove();
+            descriptions[index].remove();
+            prices[index].remove();
+            images[index].remove();
+            managePriceArray[index].remove();
+            removeArray[index].remove();
+
+            sideNames.splice(index, 1);
+            localStorage.setItem('sideNames', JSON.stringify(sideNames));
+
+            sideDescriptions.splice(index, 1);
+            localStorage.setItem('sideDescriptions', JSON.stringify(sideDescriptions));
+
+            sidePrices.splice(index, 1);
+            localStorage.setItem('sidePrices', JSON.stringify(sidePrices));
+
+            sideImages.splice(index, 1);
+            localStorage.setItem('sideImages', JSON.stringify(sideImages));
+        })
         sideBox.appendChild(remove);
     }
 
@@ -814,11 +876,12 @@ function retrieveItemsDrinksManager() {
 
     for (i in drinkNames) {
         let name = document.createElement('h4');
+        name.classList.add('item-name-drink');
         name.innerHTML = drinkNames.slice(0)[i];
         drinkBox.appendChild(name);
     
         let description = document.createElement('p');
-        description.classList.add('item-description');
+        description.classList.add('item-description-drink');
         description.innerHTML = drinkDescriptions.slice(0)[i];
         drinkBox.appendChild(description);
     
@@ -828,7 +891,7 @@ function retrieveItemsDrinksManager() {
         drinkBox.appendChild(price);
     
         let image = document.createElement('img');
-        image.classList.add('item-image');
+        image.classList.add('item-image-drink');
         image.src = drinkImages.slice(0)[i];
         drinkBox.appendChild(image);
 
@@ -851,8 +914,38 @@ function retrieveItemsDrinksManager() {
         drinkBox.appendChild(managePrice);
     
         let remove = document.createElement('button');
-        remove.classList.add('remove-btn');
+        remove.classList.add('remove-drink');
         remove.innerHTML = 'Remove';
+        remove.addEventListener('click', function() {
+            let removeArray = document.getElementsByClassName('remove-drink');
+            let names = document.getElementsByClassName('item-name-drink');
+            let descriptions = document.getElementsByClassName('item-description-drink');
+            let prices = document.getElementsByClassName('item-price-drink');
+            let images = document.getElementsByClassName('item-image-drink');
+            let managePriceArray = document.getElementsByClassName('managePrice-drink');
+
+            
+            let index = Array.prototype.indexOf.call(removeArray, this);
+
+            names[index].remove();
+            descriptions[index].remove();
+            prices[index].remove();
+            images[index].remove();
+            managePriceArray[index].remove();
+            removeArray[index].remove();
+
+            drinkNames.splice(index, 1);
+            localStorage.setItem('drinkNames', JSON.stringify(drinkNames));
+
+            drinkDescriptions.splice(index, 1);
+            localStorage.setItem('drinkDescriptions', JSON.stringify(drinkDescriptions));
+
+            drinkPrices.splice(index, 1);
+            localStorage.setItem('drinkPrices', JSON.stringify(drinkPrices));
+
+            drinkImages.splice(index, 1);
+            localStorage.setItem('drinkImages', JSON.stringify(drinkImages));
+        })
         drinkBox.appendChild(remove);
     }
 }
